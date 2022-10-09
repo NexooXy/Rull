b3268dd71080f719bfe8536970568077325466f7
NexooXy <114862689+NexooXy@users.noreply.github.com>
2022-10-09 19:13:31 +0700

    Update sticker-toimg.js

--- plugins/sticker-toimg.js
+++ plugins/sticker-toimg.js
@@ -7,10 +7,10 @@ let handler = async (m, { conn, usedPrefix, command }) => {
     if (!/sticker/.test(mime)) throw notStickerMessage
     let media = await q.download()
     let out = await webp2png(media).catch(_ => null) || Buffer.alloc(0)
-    await conn.sendFile(m.chat, out, 'out.png', '*DONE (≧ω≦)ゞ*', m)
+    await conn.sendFile(m.chat, out, 'out.png', 'Selesai', m)
 }
 handler.help = ['toimg (reply)']
 handler.tags = ['sticker']
 handler.command = ['toimg']
-
+handler.limit = true
 export default handler

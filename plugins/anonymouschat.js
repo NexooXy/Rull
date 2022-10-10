d33f5a06615f386d13c1dc4862f00e823ce4c7ad
NexooXy <114862689+NexooXy@users.noreply.github.com>
2022-10-10 19:48:04 +0700

    Update anonymouschat.js

--- plugins/anonymouschat.js
+++ plugins/anonymouschat.js
@@ -1,95 +1,38 @@
-let { MessageType } = (await import('@adiwajshing/baileys')).default
+let handler = async (m, { conn, usedPrefix: _p, __dirname, args }) => {
+let text = `
+Welcome To Anonymous Chat
 
-let handler  = async (m, { conn, command, args, usedPrefix, DevMode }) => {
-  let type = (args[0] || '').toLowerCase()
-  let _type = (args[0] || '').toLowerCase()
+Command:
+#start
+⟩ untuk memulai anonymous chat
+#skip
+⟩ Untuk skip anonymous chat
+#stop
+⟩ untuk berhenti anonymous chat
+#sendkontak
+⟩ untuk mengirim kontak/nomor ke Anon chat
 
-//------- NOMOR
-  let nowner = `${nomorown.split`@`[0]}@s.whatsapp.net`
-  let teksnomor = `
-ɴᴏᴍᴏʀ ᴏᴡɴᴇʀ ʙᴏᴛ
+Rules:
+Jangan spam
+Jangan di salah gunakan
+Jangan rasis/Toxic
 
-ʀᴜʟʟ:
-@${nomorown.split`@`[0]}
-
-
-ʀᴜʟᴇs:
-ᴊᴀɴɢᴀɴ ᴄʜᴀᴛ ᴀɴᴇʜ ᴀɴᴇʜ ᴋᴇ ᴏᴡɴᴇʀ ʙᴏᴛ
-ᴊᴀɴɢᴀɴ sᴘᴀᴍ ᴋᴇ ᴏᴡɴᴇʀ ʙᴏᴛ
-ᴊᴀɴɢᴀɴ ᴄᴀʟʟ ᴏᴡɴᴇʀ ʙᴏᴛ
-
-ᴄ ʟ ᴏ ᴜ ᴅ      ʙ ᴏ ᴛ
+Melanggar Rules di atas akan di block permanent 
 `
-
-//------------ BIO
-let ppown = await conn.profilePictureUrl(nomorown + '@s.whatsapp.net', 'image').catch(_ => hwaifu[1]) 
-let teksbio = `No Bio`
-  let teks = 'ᴘɪʟɪʜ ᴅɪ ʙᴀᴡᴀʜ'
-const sections = [
-   {
-	title: `░ ⃞ׄ🪄 ᴀɴᴏɴʏᴍᴏᴜs  !! ⥽`,
-	rows: [
-	    {title: "ᴍᴜʟᴀɪ ᴀɴᴏɴʏᴍᴏᴜs ᴄʜᴀᴛ", rowId: ".start"},
-	    {title: "ʙᴇʀʜᴇɴᴛɪ ᴀɴᴏɴʏᴍᴏᴜs ᴄʜᴀᴛ", rowId: ".leave"},
-	    {title: "ᴋɪʀɪᴍᴋᴀɴ ᴋᴏɴᴛᴀᴋ ᴍᴜ", rowId: ".sendkontak"},
-	]
-    },{
-	title: `░ ⃞ׄ🪄 ʟᴀɪɴɴʏᴀ  !! ⥽`,
-	rows: [
-	    {title: "ᴅᴏɴᴀsɪ", rowId: ".donasi"},
-	    {title: "ᴏᴡɴᴇʀ ʙᴏᴛ", rowId: ".owner"},
-	    {title: "ᴍᴇɴᴜ ʙᴏᴛ", rowId: ".menu"},
-	    {title: "ᴘɪɴɢ ʙᴏᴛ", rowId: ".ping"},
-	]
-  },
+const templateButtons = [
+    {index: 1, quickReplyButton: {displayText: '🔍 Start', id: '.start'}},
+    {index: 2, quickReplyButton: {displayText: '📍 Menu', id: '.menu'}},
 ]
-
-const listMessage = {
-  text: teks,
-  footer: null,
-  title: `ᴀɴᴏɴʏᴍᴏᴜs ᴄʜᴀᴛ\n\nᴄᴀʀɪ ᴛᴇᴍᴀɴ ʙᴀʀᴜ`,
-  buttonText: "ᴋʟɪᴋ ᴅɪsɪɴɪ !",
-  sections
+let tm = {
+text: text,
+footer: global.wm,
+templateButtons: templateButtons,
+image: {url: fla + 'Donasi'}
 }
-
-  try {
-    if (/(creator|owner)/i.test(command)) {
-      const count = args[1] && args[1].length > 0 ? Math.min(99999999, Math.max(parseInt(args[1]), 1)) : !args[1] || args.length < 3 ? 1 : Math.min(1, count)
-        switch (type) {
-          case 'nomor':
-          conn.reply(m.chat, teksnomor, m, { contextInfo: { mentionedJid: [nowner] }})
-            break
-            case 'bio':
-          //conn.sendHydrated(m.chat, teksbio, wm, ppown, sig, "📷 Instagram", nomorown, '🌹 Nomor', [[null, null], [null, null],[null,null]], m)
-
-          conn.sendButton(m.chat, teksbio, wm, ppown, [
-                ['Sewa Bot', `${usedPrefix}sewa`],
-                ['Menu', `${usedPrefix}menu`]
-            ], m)
-            break
-            
-          default:
-            return await conn.sendMessage(m.chat, listMessage, m, { contextInfo: { mentionedJid: [m.sender] }})
-        }
-    } else if (/enchant|enchan/i.test(command)) {
-      const count = args[2] && args[2].length > 0 ? Math.min(99999999, Math.max(parseInt(args[2]), 1)) : !args[2] || args.length < 4 ? 1 :Math.min(1, count)
-      switch (_type) {
-        case 't':
-          break
-        case '':
-          break
-
-        default:
-          return conn.sendButton( m.chat, caption, wm, null, [`⋮☰ Menu`, `.menu`], m)
-      }
-    }
-  } catch (err) {
-    m.reply("Error\n\n\n" + err.stack)
-  }
+conn.sendMessage(m.chat, tm, m)
 }
-
-handler.help = ['anonymous', 'anonchat']
-handler.tags = ['anonymous', 'main']
-handler.command = /^(anonymous|anonchat|chatanonymous|anonymouschat|anonchat)/i
+handler.help = ['anonymous']
+handler.tags = ['anonymous']
+handler.command = /^(anonymous|anonchat|Anon|chatanon)$/i
 
 export default handler
